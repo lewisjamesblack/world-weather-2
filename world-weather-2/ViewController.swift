5abd614d26a14ff9d2415fb19b09f3a5bb01a8b5
Lewis Black <lewisblack@lewiss-mbp-2.home>
2016-04-03 11:41:03 +0100

    Initial Commit

---
+++ world-weather-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  world-weather-2
+//
+//  Created by Lewis Black on 03/04/2016.
+//  Copyright © 2016 Lewis Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
